bb494808e4267326205a4e5433852099f7bfa5c9
Max Braun <max.braun@gmail.com>
2019-05-31 17:51:41 -0700

    Use absolute paths for directories

--- eboy_run.sh
+++ eboy_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-IMAGES_DIR=eboy-images
-DATASET_DIR=eboy-dataset
+IMAGES_DIR="$(pwd)/eboy-images"
+DATASET_DIR="$(pwd)/datasets/eboy-dataset"
 
 # Download and crop the images.
 python eboy_data.py --images_dir=$IMAGES_DIR
 
 # Generate the dataset.
-python dataset_tool.py create_from_images datasets/$DATASET_DIR $IMAGES_DIR
+python dataset_tool.py create_from_images $DATASET_DIR $IMAGES_DIR
 
 # Start training.
 python eboy_train.py --dataset_dir=$DATASET_DIR
