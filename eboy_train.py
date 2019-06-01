91c2863564c20a7aed298660e5e7de6fe6e82d99
Max Braun <max.braun@gmail.com>
2019-06-01 10:07:06 -0700

    Increase number of training cycles

--- eboy_train.py
+++ eboy_train.py
@@ -38,7 +38,7 @@ desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.mi
 
 # Default options.
 #train.total_kimg = 25000
-train.total_kimg = 125
+train.total_kimg = 1000
 sched.lod_initial_resolution = 8
 sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
 sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
